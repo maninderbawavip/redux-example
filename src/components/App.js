88f640f1827ad45578c10eb7ed793ae389f6606c
Maninder Singh Bawa <maninderbawavip@gmail.com>
2021-03-03 13:26:07 +0530

    conetxt api added

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,46 @@
+import React, { Component } from 'react';
+import { AppContext } from '../contexts/AppContext';
 import './App.css';
 import Celebs from './Celebs';
 import EditName from './EditName';
 import Search from './Search';
 import UserInfo from './UserInfo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
 
-        <Search />
-        <Celebs />
+  constructor(props){
+    super(props);
+    this.state = {
+      theme: "light"
+    }
+  }
 
-      </header>
-    </div>
-  );
+
+  changeTheme = () => {
+    this.setState({ theme: this.state.theme === "light" ? "dark": "light"})
+  }
+
+  render(){
+
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          {/* //exposed some value from provider */}
+          <AppContext.Provider value={{
+            currentTheme: this.state.theme,
+            changeTheme: this.changeTheme
+          }}>
+            <Search />
+            
+            <Celebs />
+          </AppContext.Provider>
+        </header>
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
